e65e5cae2b67108a422ab64ca48f867dd003208f
Theena-hub <theenathayalanramakrishnan@gmail.com>
2023-12-26 21:37:40 +0530

    first commit

---
+++ course.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('head.php') ?>
+</head>
+
+<body>
+    <!-- navbar - start -->
+    <div class="container-fluid">
+        <div class="d-flex justify-content-between flex-column br-10"
+            style="width: fit-content; height: 250px;position: fixed;left: 0px;top: 50%;transform: translateY(-50%);border-left: 5px solid #fff;z-index: 5;">
+            <div class="ml-5">
+                <a href="#">
+                    <img src="assets/img/homeIcon.svg" alt="cousenexgen" />
+                    <h1 class="titleFive width-fit-content color-FFFFFF fw-500">Home</h1>
+                </a>
+            </div>
+            <div class="ml-5">
+                <a href="index.php#course">
+                    <img src="assets/img/courseIcon.svg" alt="cousenexgen" />
+                    <h1 class="titleFive width-fit-content color-FFFFFF fw-500">Course</h1>
+                </a>
+            </div>
+            <div class="ml-5">
+                <a href="">
+                    <img src="assets/img/aboutIcon.svg" alt="cousenexgen" />
+                    <h1 class="titleFive width-fit-content color-FFFFFF fw-500">About</h1>
+                </a>
+            </div>
+            <div class="ml-5">
+                <a href="">
+                    <img src="assets/img/contactIcon.svg" alt="cousenexgen" />
+                    <h1 class="titleFive width-fit-content color-FFFFFF fw-500">Contact</h1>
+                </a>
+            </div>
+
+        </div>
+    </div>
+    <!-- navbar - end -->
+    <!-- home hero section - start -->
+    <!-- <div class="container-fluid homeHeroSec d-flex justify-content-center align-items-center">
+        <div class="container">
+            <img class="centerImg" src="assets/img/homeHeroSecBg.svg" alt="coursenexgen" />
+            <div class="row row-gap">
+                <div class="col-lg-7 col-md-7 col-sm-7 m-auto">
+                    <div class="left">
+                        <h1 class="titleOne fw-600 color-FFFFFF">Best Way To Improve Your Skills</h1>
+                        <p class="descriptionOne fw-400 color-FFFFFF mb-0">Unlock your full potential with
+                            our unparalleled approach to personal and professional development. Learn without limits,
+                            upskill your career, and elevate your performance with the world's most trusted training
+                            partner, dedicated to enhancing knowledge, skills, and capabilities."</p>
+                        <div class="d-flex btnBox">
+                            <input type="button" class="btn bgSecondaryColor mr-20 br-30 fw-500 color-180101"
+                                value="Explore Course" />
+                            <input type="button"
+                                class="btn bgColor-FFFFFF br-30 fw-500 secondaryColor btn-border-secondaryColor"
+                                value="Contact Us" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-5 col-sm-5 m-auto right">
+                    <div class="text-center">
+                        <img src="assets/img/homeHeroSecImg.png" alt="coursenexgen" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- home hero section - end -->
+    <!-- courseSecTwo - start -->
+    <div class="container-fluid courseSecTwo py-40">
+        <div class="container">
+            <div class="row row-gap">
+                <div class="col-lg-8 col-md-9">
+                    <div class="left">
+                        <h1 class="titleTwo fw-500 color-000000">Overview of the <span
+                                class='secondaryColor'>course</span></h1>
+                        <div class="durationBox d-flex align-items-center bgColor-FFFFFF width-fit-content br-30 mb-15">
+                            <img src="assets/img/clockIcon.png" alt="coursenexgen" />
+                            <h2 class="titleFour fw-500 primaryColor ml-10 mb-0">Program Duration: 2 Years</h2>
+                        </div>
+                        <p class="descriptionOne fw-400 color-505050">Designed to equip you with the knowledge, skills
+                            and internships necessary to excel in the dynamic fields of supply chain management &
+                            logistics. This comprehensive program facilitates immersive learning and covers key
+                            concepts, strategies, and best practices in global supply chain management, enabling you to
+                            make informed decisions and drive operational excellence.</p>
+                        <ul>
+                            <li class="descriptionOne fw-400 color-505050">2-year online PGDM approved by AICTE, Govt of
+                                India</li>
+                            <li class="descriptionOne fw-400 color-505050">Learning materials from World Bank, IMF,
+                                Harvard & Pearson</li>
+                            <li class="descriptionOne fw-400 color-505050">Gain work experience in the BFSI industry
+                                while learning</li>
+                            <li class="descriptionOne fw-400 color-505050">3 Industry-recognized certifications from
+                                State University of New York, AIMA and Expertrons Post Graduation Certification (BFSI)
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-3">
+                    <div>
+                        <img class="img-fluid" src="assets/img/courseSecTwoImg.png" alt="coursenexgen" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- courseSecTwo - end -->
+    <!-- courseSecThree - start -->
+    <div class="container-fluid courseSecThree py-40"></div>
+    <!-- courseSecThree - end -->
+    <!-- courseSecFour - start -->
+    <div class="container-fluid courseSecFour py-40 bgColor-F5F5F5">
+        <h1 class="titleTwo fw-500 color-000000 text-center">Key <span class="secondaryColor">Highlights</span> of the
+            Program</h1>
+        <div class="container mt-30">
+            <div class="row row-gap">
+                <div class="col-lg-6 col-md-6">
+                    <div class="card b-none br-8">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Certification by State University of
+                                New York</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="card b-none br-8">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Certification by State University of
+                                New York</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="card b-none br-8">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Certification by State University of
+                                New York</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="card b-none br-8">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Certification by State University of
+                                New York</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="card b-none br-8">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Certification by State University of
+                                New York</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="card b-none br-8">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Certification by State University of
+                                New York</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="card b-none br-8">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Certification by State University of
+                                New York</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="card b-none br-8">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Certification by State University of
+                                New York</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- courseSecFour - end -->
+    <!-- courseSecFive - start -->
+    <div class="container-fluid courseSecFive py-40">
+        <h1 class="titleTwo fw-500 color-000000 text-center">Enrollment <span class="secondaryColor">Process</span></h1>
+        <div class="container"></div>
+    </div>
+    <!-- courseSecFive - end -->
+    <!-- courseSecSix - start -->
+    <div class="container-fluid courseSecSix py-40 bgColor-F5F5F5"></div>
+    <!-- courseSecSix - end -->
+    <!-- courseSecSeven - start -->
+    <div class="container-fluid courseSecSeven py-40">
+        <h1 class="titleTwo fw-500 color-000000 text-center">Potential <span class="secondaryColor">Career Path</span>
+        </h1>
+        <div class="container mt-25">
+            <div class="row row-gap">
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card b-none br-40 width-fit-content">
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid" src="assets/img/tickMark.png" alt="coursenexgen" />
+                            <h3 class="titleFive fw-500 color-000000 mb-0 ml-15">Supply chain management</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- courseSecSeven - end -->
+    <!-- footer - start -->
+    <?php include('footer.php') ?>
+    <!-- footer - end -->
+    <!-- script - start -->
+    <?php include('script.php') ?>
+    <!-- script - end -->
+</body>
+
+</html>
